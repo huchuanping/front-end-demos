f805b9a70a17b8c2b19df87e76bb8747b8b4f938
huchuanping <1137213572@qq.com>
2017-09-06 11:07:02 +0800

    computer

--- demos/jq-computer/index.js
+++ demos/jq-computer/index.js
@@ -1,20 +1,20 @@
 var $ = require('jquery');
-var leftOperand;
-var rightOperand;
+var leftOperand='';
+var rightOperand='';
 var isLeftOperand=true;
 var op;
 /*点击操作数*/
 $('.operand').click(function(){
 	// debugger
 	if(isLeftOperand){
-		if(leftOperand){
+		if(leftOperand!=''){
 			leftOperand=leftOperand+$(this).text()
 		}else{
 			leftOperand=$(this).text()
 		}
 		$('.output').val(leftOperand)
 	}else{
-		if(rightOperand){
+		if(rightOperand!=''){
 			rightOperand=rightOperand+$(this).text()
 		}else{
 			rightOperand=$(this).text()
@@ -25,32 +25,37 @@ $('.operand').click(function(){
 })
 /*点击操作符*/
 $('.operator').click(function(){
-	isLeftOperand=false
 	$('.output').val('')
+	isLeftOperand=false
 	op=$(this).text()
+	console.log(rightOperand)
 	// debugger
+	
 })
 /*点击等于号计算结果*/
 $('.result').click(function(){
+	isLeftOperand=true
 	switch(op){
 		case '+' :
-		leftOperand=parseInt(leftOperand) + parseInt(rightOperand);
+		leftOperand=parseFloat(leftOperand) + parseFloat(rightOperand);
 		break;
 		case '-' :
-		leftOperand=parseInt(leftOperand) - parseInt(rightOperand);
+		leftOperand=parseFloat(leftOperand) - parseFloat(rightOperand);
 		break;
 		case '*' :
-		leftOperand=parseInt(leftOperand) * parseInt(rightOperand);
+		leftOperand=parseFloat(leftOperand) * parseFloat(rightOperand);
 		break;
 		case '/' :
-		leftOperand=parseInt(leftOperand)  / parseInt(rightOperand);
+		leftOperand=parseFloat(leftOperand)  / parseFloat(rightOperand);
 		break;
 	}
 	$('.output').val(leftOperand)
+	rightOperand=''
 })
 /*清零*/
 $('.clear').click(function(){
 	leftOperand=''
 	rightOperand=''
+	isLeftOperand=true
 	$('.output').val('')
 })
